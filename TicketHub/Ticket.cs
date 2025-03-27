b1b8ff906687e8325683de8a9769b595a9282cc8
JayHarvie <Jayharv14@gmail.com>
2025-03-26 21:12:46 -0300

    enhanced data annotations

--- TicketHub/Ticket.cs
+++ TicketHub/Ticket.cs
@@ -16,7 +16,8 @@ namespace TicketHub
         public string Name { get; set; } = string.Empty;
 
         [Required]
-        [RegularExpression(@"^(\+?\d{1,2}\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$", ErrorMessage = "Phone number must be a valid 10-digit number.")]
+        [RegularExpression(@"^(\+?\d{1,2}\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$", ErrorMessage = "Phone number must be a valid number.")]
+        [MaxLength(10, ErrorMessage = "Phone number needs to be 10 numbers.")]
         public string Phone { get; set; } = string.Empty;
 
         [Required]
@@ -37,22 +38,27 @@ namespace TicketHub
 
         [Required]
         [RegularExpression(@"^\d+\s[A-Za-z]+\s[A-Za-z]+$", ErrorMessage = "Street address must be in the format '11 Example Rd'.")]
+        [MaxLength(60, ErrorMessage = "Address cannot exceed 60 characters.")]
         public string Address { get; set; } = string.Empty;
 
         [Required]
         [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "City name can only contain letters and spaces.")]
+        [MaxLength(50, ErrorMessage = "City name cannot exceed 50 characters.")]
         public string City { get; set; } = string.Empty;
 
         [Required]
         [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Province name can only contain letters and spaces.")]
+        [MaxLength(30, ErrorMessage = "Province name cannot exceed 50 characters.")]
         public string Province { get; set; } = string.Empty;
 
         [Required]
         [RegularExpression(@"^[A-Za-z]\d[A-Za-z]\d[A-Za-z]\d$", ErrorMessage = "Postal code must be in the format A1A1A1.")]
+        [MaxLength(6, ErrorMessage = "Postal code must be 6 characters.")]
         public string PostalCode { get; set; } = string.Empty;
 
         [Required]
         [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Country name can only contain letters and spaces.")]
+        [MaxLength(50, ErrorMessage = "Country name cannot exceed 50 characters.")]
         public string Country { get; set; } = string.Empty;
     }
 }
